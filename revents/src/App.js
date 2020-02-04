35e577fd72579dd0e96310d8fa854cee0dc266ff
Abdu Qnauy <abduqnauy@gmail.com>
2020-02-05 01:41:28 +0200

    End of section 3

--- revents/src/App.js
+++ revents/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import EventDashboard from "./components/event/EventDashboard/EventDashboard";
+import NavBar from "./components/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>re-events</h1>
-    </div>
+    <>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    </>
   );
 }
 
