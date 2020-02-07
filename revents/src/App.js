ee795a9f25d078c8d5b53914f1f5442516a4476c
Abdu Qnauy <abduqnauy@gmail.com>
2020-02-07 02:57:49 +0200

    End of section 6

--- revents/src/App.js
+++ revents/src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+//
 import EventDashboard from "./components/event/EventDashboard/EventDashboard";
 import NavBar from "./components/nav/NavBar/NavBar";
-import { Container } from "semantic-ui-react";
-
+import HomePage from "./components/home/HomePage";
+import EventDetailedPage from "./components/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "./components/user/PeopleDashboard/PeopleDashboard";
+import UserDetailedPage from "./components/user/UserDetailed/UserDetailedPage";
+import SettingsDashboard from "./components/user/Settings/SettingsDashboard";
+import EventForm from "./components/event/EventForm/EventForm";
+//
 function App() {
   return (
     <>
-      <NavBar />
-      <Container className="main">
-        <EventDashboard />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path="/(.+)"
+        render={() => (
+          <>
+            <NavBar />
+            <Container className="main">
+              <Route exact path="/events" component={EventDashboard} />
+              <Route path="/events/:id" component={EventDetailedPage} />
+              <Route path="/people" component={PeopleDashboard} />
+              <Route path="/profile/:id" component={UserDetailedPage} />
+              <Route path="/settings" component={SettingsDashboard} />
+              <Route path="/createEvent" component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
